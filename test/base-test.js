6f39db943cf886d1bd8d77837773cc27e7ec73ad
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2016-09-20 09:52:15 +0100

    closes #23 | Added code coverage with coveralls (#46)
    * code-coverage#23 | Added gitignore for node
    
    * issue #23 | Added code coverage config

--- test/base-test.js
+++ test/base-test.js
@@ -1,7 +1,9 @@
 var expect = require("expect.js");
+var Index = require("../src/index.js")
+var index = new Index();
 
 describe("Base test", function() {
   it("should always pass this test", function() {
-    expect(true).to.be(true);
+    expect(index.value).to.be(true);
   });
 });

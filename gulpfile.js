e19bb09820f752c4def1f8c628c8e9f2d1ba17a5
Ologho Cyril Paul <cyrielo911@gmail.com>
2016-09-22 21:10:47 +0100

    Updates gulp task, and package.json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-"use strict";
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('eslint', ()=>{
-   return gulp.src(['src/*.js','!node_modules/**'])
+   return gulp.src(['src/*.js', 'gulpfile.js', '!node_modules/**'])
        .pipe( eslint({configFile: '.eslintrc'}) )
        .pipe(eslint.format());
 });

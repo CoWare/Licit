ec66a0ad0d7e65f518882b5c0277bf88f9f96e1c
Cyril Paul <cyrielo911@GMAIL.COM>
2016-09-23 09:33:00 +0100

    Fixes #48 | Add gulpfile (#49)
    * Adds gulp task for eslint
    
    * Updates gulp task, and package.json file
    
    * Updates code indenting, updates package.json

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', () => {
+  return gulp.src(['src/*.js', 'gulpfile.js', '!node_modules/**'])
+    .pipe( eslint({configFile: '.eslintrc'}) )
+    .pipe(eslint.format());
+});

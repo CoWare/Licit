278797389a6f4df6e185861072b8e07710053360
Ologho Cyril Paul <cyrielo911@gmail.com>
2016-09-23 08:48:30 +0100

    Updates code indenting, updates package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
-gulp.task('eslint', ()=>{
-   return gulp.src(['src/*.js', 'gulpfile.js', '!node_modules/**'])
-       .pipe( eslint({configFile: '.eslintrc'}) )
-       .pipe(eslint.format());
+gulp.task('lint', () => {
+  return gulp.src(['src/*.js', 'gulpfile.js', '!node_modules/**'])
+    .pipe( eslint({configFile: '.eslintrc'}) )
+    .pipe(eslint.format());
 });
